1f4b3968640390da7c2cc9aedc6165c3029c9f72
Florian DAL FITTO <xillibit@gmail.com>
2011-11-01 20:50:28 +0100

    # [#21] Kunena Latest Messed Up First Parameter

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -32,6 +32,7 @@ KunenaLatest 1.7.1-DEV
 
 1-November-2011 Xillibit
 # [#24] Remove setting horizontal/vertical display
+# [#21] Kunena Latest Messed Up First Parameter
 
 9-October-2011 Xillibit
 ^ [#17] Follow Kunena date format settings
