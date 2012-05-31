e63b7cddadf0d058a4fc8abd3df81f9ded5baaf8
Matias Griese <matias@kunena.org>
2012-05-31 10:38:25 +0300

    Fix broken language strings in plg_community_mykunena

--- plugins/community/mykunena/mykunena.php
+++ plugins/community/mykunena/mykunena.php
@@ -85,8 +85,8 @@ class plgCommunityMyKunena extends CApplications {
 			?>
 				<li>
 					<div class="content">
-						<a href="<?php echo KunenaRoute::_ ( "index.php?option=com_kunena&view=topic&catid={$item->catid}&id={$item->thread}&mesid={$item->id}" ); ?>" class="kjsubject"><?php echo $item->getTopic()->displayField('subject'); ?></a> <?php echo JText::_('PLG_MYKUNENA_POST_IN'); ?>
-						<a href="<?php echo KunenaRoute::_ ( "index.php?option=com_kunena&view=category&catid={$item->catid}" ); ?>" class="kjcategory"><?php echo $item->getCategory()->displayField('name'); ?></a> <?php echo JText::_('PLG_MYKUNENA_POST_ON'); ?>
+						<a href="<?php echo KunenaRoute::_ ( "index.php?option=com_kunena&view=topic&catid={$item->catid}&id={$item->thread}&mesid={$item->id}" ); ?>" class="kjsubject"><?php echo $item->getTopic()->displayField('subject'); ?></a> <?php echo JText::_('PLG_COMMUNITY_MYKUNENA_POST_IN'); ?>
+						<a href="<?php echo KunenaRoute::_ ( "index.php?option=com_kunena&view=category&catid={$item->catid}" ); ?>" class="kjcategory"><?php echo $item->getCategory()->displayField('name'); ?></a> <?php echo JText::_('PLG_COMMUNITY_MYKUNENA_POST_ON'); ?>
 						<span class="kjdate"><?php echo $postDate->toFormat ( JText::_ ( 'DATE_FORMAT_LC2' ) ); ?></span>
 					</div>
 				</li>
