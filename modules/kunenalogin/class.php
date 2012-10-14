f54360c8932655c66f6562275a45e2581ca25540
Florian DAL FITTO <xillibit@gmail.com>
2012-10-14 13:08:15 +0200

    Remove useless call for kunena.link.class.php

--- modules/kunenalogin/class.php
+++ modules/kunenalogin/class.php
@@ -15,7 +15,6 @@ class ModKunenaLogin {
 	protected $params = null;
 
 	function __construct($module, $params) {
-		require_once (KPATH_SITE . '/lib/kunena.link.class.php');
 		$this->params = $params;
 	}
 
