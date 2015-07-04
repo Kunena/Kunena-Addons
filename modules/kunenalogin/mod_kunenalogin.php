231dce08a8651406e88c54c8832120c825142402
Jelle Kok <schumacher1986@hotmail.com>
2015-07-04 23:22:39 +0200

    K4.0: Refactor kunena login module

--- modules/kunenalogin/mod_kunenalogin.php
+++ modules/kunenalogin/mod_kunenalogin.php
@@ -1,23 +1,28 @@
 <?php
 /**
  * Kunena Login Module
- * @package Kunena.mod_kunenalogin
+ *
+ * @package       Kunena.mod_kunenalogin
  *
  * @copyright (C) 2008 - 2015 Kunena Team. All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.kunena.org
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link          http://www.kunena.org
  **/
-defined ( '_JEXEC' ) or die ();
+defined('_JEXEC') or die ();
 
 // Kunena detection and version check
 $minKunenaVersion = '3.0';
-if (!class_exists('KunenaForum') || !KunenaForum::isCompatible($minKunenaVersion)) {
+if (!class_exists('KunenaForum') || !KunenaForum::isCompatible($minKunenaVersion))
+{
 	echo JText::sprintf('MOD_KUNENALOGIN_KUNENA_NOT_INSTALLED', $minKunenaVersion);
+
 	return;
 }
 // Kunena online check
-if (!KunenaForum::enabled()) {
+if (!KunenaForum::enabled())
+{
 	echo JText::_('MOD_KUNENALOGIN_KUNENA_OFFLINE');
+
 	return;
 }
 
