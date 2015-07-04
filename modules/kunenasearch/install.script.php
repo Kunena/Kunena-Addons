1341fa9a5890260416ec3add031d559b9ef045d6
Jelle Kok <schumacher1986@hotmail.com>
2015-07-04 23:24:25 +0200

    K4.0: Reformat kunena search

--- modules/kunenasearch/install.script.php
+++ modules/kunenasearch/install.script.php
@@ -1,20 +1,24 @@
 <?php
 /**
  * Kunena Search Module
- * @package Kunena.mod_kunenasearch
+ *
+ * @package       Kunena.mod_kunenasearch
  *
  * @copyright (C) 2008 - 2015 Kunena Team. All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.kunena.org
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link          http://www.kunena.org
  **/
-defined ( '_JEXEC' ) or die ();
+defined('_JEXEC') or die ();
 
-class Mod_KunenasearchInstallerScript {
-	function postflight($type, $parent) {
+class Mod_KunenasearchInstallerScript
+{
+	function postflight($type, $parent)
+	{
 		// Delete useless manifest file
 		$path = $parent->getParent()->getPath('extension_root');
 		$name = $parent->get('name');
-		if (JFile::exists("{$path}/{$name}.j25.xml")) {
+		if (JFile::exists("{$path}/{$name}.j25.xml"))
+		{
 			JFile::delete("{$path}/{$name}.j25.xml");
 		}
 	}
