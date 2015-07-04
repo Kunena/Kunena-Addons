9be4fd50469965d5bc7f99752fcbdbea25b4c1c7
Jelle Kok <schumacher1986@hotmail.com>
2015-07-04 23:27:58 +0200

    K4.0: Reformat kunena stats

--- modules/kunenastats/install.script.php
+++ modules/kunenastats/install.script.php
@@ -1,20 +1,24 @@
 <?php
 /**
  * Kunena Statistics Module
- * @package Kunena.mod_kunenastats
+ *
+ * @package       Kunena.mod_kunenastats
  *
  * @copyright (C) 2008 - 2015 Kunena Team. All rights reserved.
- * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
- * @link http://www.kunena.org
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link          http://www.kunena.org
  **/
-defined( '_JEXEC' ) or die();
+defined('_JEXEC') or die();
 
-class Mod_KunenastatsInstallerScript {
-	function postflight($type, $parent) {
+class Mod_KunenastatsInstallerScript
+{
+	function postflight($type, $parent)
+	{
 		// Delete useless manifest file
 		$path = $parent->getParent()->getPath('extension_root');
 		$name = $parent->get('name');
-		if (JFile::exists("{$path}/{$name}.j25.xml")) {
+		if (JFile::exists("{$path}/{$name}.j25.xml"))
+		{
 			JFile::delete("{$path}/{$name}.j25.xml");
 		}
 	}
