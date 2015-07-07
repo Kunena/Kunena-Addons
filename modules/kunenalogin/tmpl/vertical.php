798c07b8a31d6afb6b9871e8b1b9ce8e5bf0375f
Jelle Kok <schumacher1986@hotmail.com>
2015-07-07 21:53:12 +0200

    Login module: Update style

--- modules/kunenalogin/tmpl/vertical.php
+++ modules/kunenalogin/tmpl/vertical.php
@@ -25,7 +25,7 @@ defined('_JEXEC') or die();
 					<?php echo JText::sprintf('MOD_KUNENALOGIN_HINAME', '<strong>' . $this->me->getLink($this->me->getName()) . '</strong>', $this->me->getName()); ?>
 				</div>
 			<?php endif; ?>
-			<div class="klogin-avatar">
+			<div class="img-rounded">
 				<?php if ($this->params->get('showav'))
 				{
 					echo $this->kunenaAvatar($this->me->userid);
@@ -43,9 +43,6 @@ defined('_JEXEC') or die();
 					</div>
 				<?php endif; ?>
 			</div>
-			<div class="klogin-links">
-				<input type="submit" name="Submit" class="kbutton" value="<?php echo JText::_('MOD_KUNENALOGIN_BUTTON_LOGOUT'); ?>" />
-			</div>
 			<div>
 				<ul class="klogin-loginlink">
 					<?php if ($this->privateMessages) : ?>
@@ -62,6 +59,9 @@ defined('_JEXEC') or die();
 					<?php endif; ?>
 				</ul>
 			</div>
+			<div class="klogin-links">
+				<input type="submit" name="Submit" class="btn btn-default btn-primary" value="<?php echo JText::_('MOD_KUNENALOGIN_BUTTON_LOGOUT'); ?>" />
+			</div>
 		</form>
 
 	<?php else : ?>
@@ -74,38 +74,80 @@ defined('_JEXEC') or die();
 			<?php echo JHTML::_('form.token'); ?>
 
 			<?php echo $this->params->get('pretext'); ?>
-			<fieldset class="input">
-				<p class="klogin-form-login-username">
-					<label for="klogin-username"><?php echo JText::_('MOD_KUNENALOGIN_USERNAME') ?></label>
-					<input id="klogin-username" class="klogin-username" type="text" name="username" class="kinputbox" alt="username" size="18" />
-				</p>
-				<p class="klogin-form-login-password">
-					<label for="klogin-passwd"><?php echo JText::_('MOD_KUNENALOGIN_PASSWORD') ?></label>
-					<input id="klogin-passwd" class="klogin-passwd" type="password" name="password" class="kinputbox" size="18" alt="password" />
-				</p>
-				<?php if ($this->remember) : ?>
-					<p class="klogin-form-login-remember"><label for="klogin-remember">
-							<input id="klogin-remember" class="klogin-remember" type="checkbox" name="remember" value="yes" alt="<?php echo JText::_('MOD_KUNENALOGIN_REMEMBER_ME') ?>" />
-							<?php echo JText::_('MOD_KUNENALOGIN_REMEMBER_ME') ?></label>
+			<div class="userdata">
+				<div id="form-login-username" class="control-group">
+					<div class="controls">
+						<div class="input-prepend">
+							<span class="add-on">
+								<span class="icon-user hasTooltip" title="<?php echo JText::_('MOD_KUNENALOGIN_USERNAME') ?>"></span>
+								<label for="modlgn-username" class="element-invisible"><?php echo JText::_('MOD_KUNENALOGIN_USERNAME'); ?></label>
+							</span>
+							<input id="modlgn-username" type="text" name="username" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('MOD_KUNENALOGIN_USERNAME') ?>" />
+						</div>
+					</div>
+				</div>
+				<div id="form-login-password" class="control-group">
+					<div class="controls">
+						<div class="input-prepend">
+							<span class="add-on">
+								<span class="icon-lock hasTooltip" title="<?php echo JText::_('MOD_KUNENALOGIN_PASSWORD') ?>"></span>
+								<label for="modlgn-passwd" class="element-invisible"><?php echo JText::_('MOD_KUNENALOGIN_PASSWORD'); ?></label>
+							</span>
+							<input id="modlgn-passwd" type="password" name="password" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('MOD_KUNENALOGIN_PASSWORD') ?>" />
+						</div>
+					</div>
+				</div>
+				<?php $login = KunenaLogin::getInstance(); ?>
+				<?php if ($login->getTwoFactorMethods() > 1) : ?>
+					<div id="form-login-secretkey" class="control-group">
+						<div class="controls">
+							<div class="input-prepend input-append">
+								<span class="add-on">
+									<span class="icon-star hasTooltip" title="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>"></span>
+									<label for="modlgn-secretkey" class="element-invisible"><?php echo JText::_('JGLOBAL_SECRETKEY'); ?></label>
+								</span>
+								<input id="modlgn-secretkey" autocomplete="off" type="text" name="secretkey" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('JGLOBAL_SECRETKEY') ?>" />
+								<span class="btn width-auto hasTooltip" title="<?php echo JText::_('JGLOBAL_SECRETKEY_HELP'); ?>">
+									<span class="icon-help"></span>
+								</span>
+							</div>
+						</div>
+					</div>
+				<?php endif; ?>
+				<?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
+					<div id="form-login-remember" class="control-group center">
+						<div class="controls">
+							<div class="input-prepend input-append">
+								<input id="login-remember" type="checkbox" name="remember" class="inputbox" value="yes" />
+								<label for="login-remember" class="control-label">
+									<?php echo JText::_('MOD_KUNENALOGIN_REMEMBER_ME'); ?>
+								</label>
+							</div>
+						</div>
+					</div>
+				<?php endif; ?>
+				<div id="form-login-submit" class="control-group center">
+					<p>
+						<button type="submit" tabindex="3" name="submit" class="btn btn-primary btn">
+							<?php echo JText::_('MOD_KUNENALOGIN_BUTTON_LOGIN'); ?>
+						</button>
 					</p>
-				<?php endif; ?>
-				<input type="submit" name="Submit" class="kbutton" value="<?php echo JText::_('MOD_KUNENALOGIN_BUTTON_LOGIN') ?>" />
-			</fieldset>
-			<ul class="klogin-logoutlink">
-				<li class="klogin-forgotpass">
-					<a href="<?php echo $this->lostPasswordUrl ?>" rel="nofollow"><?php echo JText::_('COM_KUNENA_PROFILEBOX_FORGOT_PASSWORD') ?></a>
-				</li>
-				<li class="klogin-forgotname">
-					<a href="<?php echo $this->lostUsernameUrl ?>" rel="nofollow"><?php echo JText::_('COM_KUNENA_PROFILEBOX_FORGOT_USERNAME') ?></a>
-				</li>
-				<?php if ($this->registerUrl) : ?>
-					<li class="klogin-register">
+
+					<p>
+						<a href="<?php echo $this->lostPasswordUrl ?>" rel="nofollow"><?php echo JText::_('COM_KUNENA_PROFILEBOX_FORGOT_PASSWORD') ?></a>
+							<br />
+
+						<a href="<?php echo $this->lostUsernameUrl ?>" rel="nofollow"><?php echo JText::_('COM_KUNENA_PROFILEBOX_FORGOT_USERNAME') ?></a>
+						<br />
+
+						<?php if ($this->registerUrl) : ?>
 						<a href="<?php echo $this->registerUrl ?>" rel="nofollow"><?php echo JText::_('COM_KUNENA_PROFILEBOX_CREATE_ACCOUNT') ?></a>
-					</li>
-				<?php endif; ?>
-			</ul>
-			<?php echo $this->params->get('posttext'); ?>
+						<?php endif; ?>
+					</p>
+				</div>
+
+				<?php echo $this->params->get('posttext'); ?>
+			</div>
 		</form>
 	<?php endif; ?>
-
 </div>
