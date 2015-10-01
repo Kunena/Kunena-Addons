a34ff569a786444236a906997a7ed972b8fd88e5
Jelle Kok <schumacher1986@hotmail.com>
2015-10-01 13:34:57 +0200

    Discuss plugin required field not red #180

--- plugins/content/kunenadiscuss/tmpl/form.php
+++ plugins/content/kunenadiscuss/tmpl/form.php
@@ -8,6 +8,7 @@
  * @link http://www.kunena.org
  **/
 defined( '_JEXEC' ) or die ();
+JHTML::_('behavior.formvalidator');
 ?>
 <div id="kdiscuss-quick-post<?php echo $row->id ?>" class="kdiscuss-form">
 	<div class="kdiscuss-title"><?php echo JText::_('PLG_KUNENADISCUSS_DISCUSS') ?></div>
@@ -19,9 +20,23 @@ defined( '_JEXEC' ) or die ();
 			<tr>
 				<td valign="top">
 					<table>
+					<!-- ADDED GUEST ALERT-->
+					<?php if(!$this->user->exists()) : ?>
+					<tr>
+						<td></td>
+					</tr>
+					<tr>
+						<td>
+							<div class="alert alert-info"><?php echo JText::_('COM_KUNENA_GEN_GUEST'); ?></div>
+						</td>
+					</tr>
+					<?php endif; ?>
+						<!-- END ADDITION -->
 					<tr>
 						<td><span class="kdiscuss-quick-post-label"><?php echo JText::_('PLG_KUNENADISCUSS_NAME') ?></span></td>
-						<td><input type="text" name="name" value="<?php echo $this->name ?>" <?php if ($this->user->exists()) echo 'disabled="disabled" '; ?>/></td>
+					</tr>
+					<tr>
+						<td><input type="text" name="name" value="<?php echo $this->name ?>" <?php if ($this->user->exists()) echo 'disabled="disabled" '; ?> required/></td>
 					</tr>
 					<?php if(!$this->user->exists() && $this->config->askemail) : ?>
 					<tr>
@@ -34,7 +49,7 @@ defined( '_JEXEC' ) or die ();
 						<?php echo JText::_('PLG_KUNENADISCUSS_MESSAGE') ?></span>
 					</tr>
 					<tr>
-						<td colspan="2"><textarea name="message" rows="5" cols="60" class="ktext"><?php echo $this->message ?></textarea></td>
+						<td colspan="2"><textarea name="message" rows="5" cols="60" class="ktext" required><?php echo $this->message ?></textarea></td>
 					</tr>
 					<?php if ($this->hasCaptcha()) : ?>
 					<tr>
@@ -43,7 +58,7 @@ defined( '_JEXEC' ) or die ();
 					</tr>
 					<?php endif; ?>
 					<tr>
-						<td><input type="submit" class="kbutton" value="<?php echo JText::_('PLG_KUNENADISCUSS_SUBMIT') ?>" /></td>
+						<td><input type="submit" class="kbutton btn" value="<?php echo JText::_('PLG_KUNENADISCUSS_SUBMIT') ?>" /></td>
 					</tr>
 					</table>
 				</td>
