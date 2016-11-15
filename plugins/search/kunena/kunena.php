d0953b223751220c929d1be5659e24acb6fdb8b3
Jelle Kok <info@jellekoksolutions.nl>
2016-11-15 14:41:18 +0100

    Increase min version Kunena

--- plugins/search/kunena/kunena.php
+++ plugins/search/kunena/kunena.php
@@ -11,7 +11,7 @@
 defined('_JEXEC') or die();
 
 // Kunena detection and version check
-$minKunenaVersion = '3.0';
+$minKunenaVersion = '5.0';
 
 if (!class_exists('KunenaForum') || !KunenaForum::isCompatible($minKunenaVersion))
 {
