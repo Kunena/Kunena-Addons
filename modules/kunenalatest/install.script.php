c0df5be190cfcf93f601753e7b71a2e251d20ca7
xillibit <xillibit@gmail.com>
2019-06-04 22:01:30 +0200

    Make possible to install Kunena lastest on J4.0

--- modules/kunenalatest/install.script.php
+++ modules/kunenalatest/install.script.php
@@ -12,15 +12,5 @@ defined('_JEXEC') or die();
 
 class Mod_KunenalatestInstallerScript
 {
-	function postflight($type, $parent)
-	{
-		// Delete useless manifest file
-		$path = $parent->getParent()->getPath('extension_root');
-		$name = $parent->get('name');
 
-		if (JFile::exists("{$path}/{$name}.j25.xml"))
-		{
-			JFile::delete("{$path}/{$name}.j25.xml");
-		}
-	}
 }
