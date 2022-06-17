bd826c52116ed72abb026804bf6fdee929134d1e
Ruud <info@onlinecommunityhub.nl>
2022-06-17 17:48:54 +0200

    Change: add installer (helper) functionality (#295)
    * Change: add installer (helper) functionality
    
    * temp remove preflight as helper is required but is not available in installed (pre this) version

--- plugins/content/kunenadiscuss/install.script.php
+++ plugins/content/kunenadiscuss/install.script.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Kunena Discuss Plugin
  *
@@ -8,8 +9,10 @@
  * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link          https://www.kunena.org
  **/
-defined('_JEXEC') or die();
 
+use Kunena\Plugin\Content\Kunenadiscuss\Helper\KunenaDiscussInstallerHelper;
+
+defined('_JEXEC') or die();
 /**
  * @package     Kunena
  *
@@ -18,13 +21,98 @@ defined('_JEXEC') or die();
 class PlgContentKunenadiscussInstallerScript
 {
 	/**
-	 * @param $type
-	 * @param $parent
-	 *
-	 *
-	 * @since version
+	 * @var string
 	 */
-	function postflight($type, $parent)
+	protected $installedVersion;
+
+	/**
+	 * @var array
+	 */
+	protected $maintenanceVariables;
+
+	/**
+	 * @var array
+	 */
+	protected $preflightVariables;
+
+	/**
+	 * Method to run before the install routine.
+	 *
+	 * @param   string                      $type    The action being performed
+	 * @param   JInstallerAdapterComponent  $parent  The class calling this method
+	 *
+	 * @return  void|boolean
+	 */
+	public function preflight($type, $parent)
 	{
+	}
+
+	/**
+	 * Code to execute on plugin update, used for cleaning left-overs from previous versions
+	 *
+	 * @param   object  $adapter  Adapter instance
+	 *
+	 * @return  void
+	 */
+	public function update($adapter)
+	{
+		$newVersion = $adapter->manifest->version;
+
+		// Load all maintenance variables
+		$this->setMaintenanceVariables();
+
+		// Rename all configured files
+		KunenaDiscussInstallerHelper::doMaintenance($this->maintenanceVariables, $this->installedVersion);
+	}
+
+	/**
+	 * Set the maintenance variables
+	 *
+	 * @return void
+	 */
+	public function setMaintenanceVariables()
+	{
+		$this->maintenanceVariables['rename_files'] = [];
+
+		$this->maintenanceVariables['delete_files'] = [];
+
+		$this->maintenanceVariables['remove_directories'] = [];
+
+		$this->maintenanceVariables['installation_messages'] = [];
+
+		$this->maintenanceVariables['component_warnings'] = [];
+
+		$this->maintenanceVariables['update_sites'] = [];
+	}
+
+	/**
+	 * Set the maintenance variables
+	 *
+	 * @return void
+	 */
+	public function setPreFlightMaintenanceVariables()
+	{
+		$this->preflightVariables['remove_directories'] = [
+			[
+				'folder' => JPATH_SITE . '/plugins/content/kunenadiscuss/css',
+				'version' => '6.0.1',
+				'compare' => '<'
+			],
+			[
+				'folder' => JPATH_SITE . '/plugins/content/kunenadiscuss/language',
+				'version' => '6.0.1',
+				'compare' => '<'
+			],
+			[
+				'folder' => JPATH_SITE . '/plugins/content/kunenadiscuss/tmpl',
+				'version' => '6.0.1',
+				'compare' => '<'
+			],
+			[
+				'folder' => JPATH_SITE . '/media/plg_content_kunenadiscuss',
+				'version' => '6.0.1',
+				'compare' => '<'
+			],
+		];
 	}
 }
