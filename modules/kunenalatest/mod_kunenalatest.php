969d6c061ef95685084e7be013f601faeb95c73f
Florian DAL FITTO <xillibit@gmail.com>
2024-03-20 22:00:12 +0100

    Klatest : set back Kunena 6.2

--- modules/kunenalatest/mod_kunenalatest.php
+++ modules/kunenalatest/mod_kunenalatest.php
@@ -17,7 +17,7 @@ use Kunena\Forum\Libraries\Forum\KunenaForum;
 use Kunena\Module\KunenaLatest\Site\ModuleKunenaLatest;
 
 // Kunena detection and version check
-$minKunenaVersion = '6.3';
+$minKunenaVersion = '6.2';
 
 if (!class_exists('Kunena\Forum\Libraries\Forum\KunenaForum') || !KunenaForum::isCompatible($minKunenaVersion)) {
     echo Text::sprintf('MOD_KUNENALATEST_KUNENA_NOT_INSTALLED', $minKunenaVersion);
