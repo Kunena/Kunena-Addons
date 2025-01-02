d19162cb47119380256d46566e781fa69c3412aa
Astrid van Eerd <Pinkeltje@users.noreply.github.com>
2025-01-02 15:25:41 +0100

    Make sure users stay on same page after login (#321)
    * Update kunenadiscuss.xml for version 6.0.6
    
    * Update default_login.php to return to article after login
    
    * Update kunenadiscuss.xml - copyright and remove css folder

--- plugins/content/kunenadiscuss/tmpl/default_login.php
+++ plugins/content/kunenadiscuss/tmpl/default_login.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Kunena Discuss Plugin
  *
@@ -9,17 +8,25 @@
  * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link          https://www.kunena.org
  **/
-
 defined('_JEXEC') or die('');
-
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Router\Route;
 use Kunena\Forum\Libraries\Login\KunenaLogin;
+
+// Get current URL for redirect
+$uri = Uri::getInstance();
+$return = base64_encode($uri->toString());
+
+// Get login URL and append return parameter
+$loginUrl = KunenaLogin::getInstance()->getLoginURL();
+$loginUrl .= (strpos($loginUrl, '?') === false ? '?' : '&') . 'return=' . $return;
 ?>
 <div class="kunenadiscuss kpublic pt-3 pb-3">
     <div class="kdiscuss-title login-discuss">
         <h3><?php echo Text::_('PLG_KUNENADISCUSS_DISCUSS_THIS_ARTICLE'); ?></h3>
     </div>
-    <a class="klogin-to-discuss btn btn-outline-primary" rel="nofollow" href="<?php echo KunenaLogin::getInstance()->getLoginURL(); ?>">
+    <a class="klogin-to-discuss btn btn-outline-primary" rel="nofollow" href="<?php echo $loginUrl; ?>">
         <?php echo Text::_('PLG_KUNENADISCUSS_LOG_IN_TO_COMMENT'); ?>
     </a>
 </div>
