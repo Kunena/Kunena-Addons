49197380141c4acd755512bbd1020c85ad5233a3
xillibit <xillibit@gmail.com>
2025-06-14 09:50:08 +0200

    Update copyright date to 2025

--- modules/kunenasearch/tmpl/default.php
+++ modules/kunenasearch/tmpl/default.php
@@ -5,7 +5,7 @@
  *
  * @package       Kunena.mod_kunenasearch
  *
- * @Copyright (C) 2008 - 2024 Kunena Team. All rights reserved.
+ * @Copyright (C) 2008 - 2025 Kunena Team. All rights reserved.
  * @license       http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link          https://www.kunena.org
  **/
